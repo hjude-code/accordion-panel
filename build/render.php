fce49eeeceebdb6387e3abc1560c2b2bc3c1128c
Harrison Jude <harrison@hjude.com>
2024-08-04 17:36:10 -0400

    smoothed easing in transitions

--- build/render.php
+++ build/render.php
@@ -11,8 +11,10 @@
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<a class="panel-header">
 		<span class="accordionRule accordionRule-left"></span>
-		<h2 class="panel-heading panel-heading-close"> <?php echo $panelHeading ?> </h2>
-		<h2 class="panel-heading panel-heading-open"> <?php echo $panelHeading ?> </h2>
+		<span class="panel-headings">
+			<h2 class="panel-heading panel-heading-close"> <?php echo $panelHeading ?> </h2>
+			<h2 class="panel-heading panel-heading-open"> <?php echo $panelHeading ?> </h2>
+		</span>
 		<span class="accordionRule accordionRule-right"></span>
 	</a>
 	<div class="panel-content">
