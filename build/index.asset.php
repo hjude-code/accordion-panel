cb0072dade82e39b5e1d5135c2ac3fdbbf4f3dac
hjude-design <harrison@hjude.com>
2024-08-04 20:01:41 -0400

    removed min height controls

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fa5ec2cc3410bb924687');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2ef44d0db6e5933260c6');
