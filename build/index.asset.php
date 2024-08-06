3139ed5f8f1071963bf4602c93eb650242984e31
Harrison Jude <harrison@hjude.com>
2024-08-06 11:55:42 -0400

    panel heading now updates color in editor when selected

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b2d4a0f4fd580637ba1a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '59b2b6373f1097f1b568');
