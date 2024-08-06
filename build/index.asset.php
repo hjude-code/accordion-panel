b426699c3b0758b94f2d1c58f424423b5c73f507
Harrison Jude <harrison@hjude.com>
2024-08-06 09:44:48 -0400

    added padding block attribute

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '275f806b3a8cac545e8b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4c055636dbe4adc2d523');
