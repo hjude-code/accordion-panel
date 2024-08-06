ab48db8e9d4d53ce586404afb780b2e947afd6dc
Harrison Jude <harrison@hjude.com>
2024-08-06 11:54:57 -0400

    fixed display issue that prevented panel showing in vertical orientation in editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8ee8fbd7c24056ea3684');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b2d4a0f4fd580637ba1a');
